900d23aa91ac152a4b5c37d37f61971c0ead0f9d
KaiqueMonteiro <161174025+Kmm9889@users.noreply.github.com>
2024-03-17 11:42:22 -0300

    Finalizando Lab2

---
+++ Labs/Lab2/Exercicio1.bat.bat
@@ -0,0 +1,6 @@
+cd C:\Users\kaiqu\ICS
+IF NOT EXIST Lab2 mkdir Lab2
+cd Lab2
+mkdir backup
+echo 1>exercicio1.txt
+copy exercicio1.txt backup/backup.exercicio1.txt
---
+++ Labs/Lab2/Exercicio2.bat.bat
@@ -0,0 +1,8 @@
+cd C:\Users\kaiqu\ICS
+IF NOT EXIST Lab2 mkdir Lab2
+cd Lab2
+mkdir Nomes
+cd Nomes
+echo Maria Joaquina Joana Dora Ana  1>nomes_meninas.txt
+echo JoÒo Jose Kaique Marcos Mario  1>nomes_meninos.txt
+find "Maria" *.txt  1>busca.txt
