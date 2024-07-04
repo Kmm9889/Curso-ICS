5df84f674cc51f8b36f8da75820f30e610fe42fc
KaiqueMonteiro <161174025+Kmm9889@users.noreply.github.com>
2024-07-04 20:21:30 -0300

    ex19-28

---
+++ teste/exercicio_19_28/exercicio20.py
@@ -0,0 +1,5 @@
+def soma(num1, num2):
+    return num1 + num2
+
+resultado = soma(30, 67)
+print(resultado)
---
+++ teste/exercicio_19_28/exercicio21.py
@@ -0,0 +1,5 @@
+def conta_caracteres(string):
+    return len(string)
+
+res = conta_caracteres("Arraial dos Buritis")
+print(res)
